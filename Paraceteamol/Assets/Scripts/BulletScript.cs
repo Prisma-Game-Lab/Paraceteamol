8d42b169329bf07fccdf31cc3153d484ae31ff6a
Hugo Tonette <hugo.tonette@gmail.com>
2019-09-05 22:28:13 -0300

    fix no uso de controles com 2 players

--- Paraceteamol/Assets/Scripts/BulletScript.cs
+++ Paraceteamol/Assets/Scripts/BulletScript.cs
@@ -4,33 +4,31 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    [Tooltip("Velocidade com que a bola é lançada quando está com o player")]
-    public float Speed = 20;
-    [HideInInspector]
-    public Rigidbody2D Rb;
+	[Tooltip("Velocidade com que a bola é lançada quando está com o player")]
+	public float Speed = 20;
+	[HideInInspector]
+	public Rigidbody2D Rb;
 
-    private Rigidbody2D _target;
-    private BallShoot ShootScript; //Usamos isso para poder setar no outro codigo que o jogador tem a bola
+	private Rigidbody2D _target;
+	private BallShoot ShootScript; //Usamos isso para poder setar no outro codigo que o jogador tem a bola
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Rb.velocity = transform.up * Speed;
+	void Start()
+	{
+		Rb.velocity = transform.up * Speed;
 
-        GameObject _player = GameObject.FindGameObjectWithTag("Player"); //ele pega o jogador para poder referenciar o script
-        ShootScript = _player.GetComponent<BallShoot>();
-         
+		GameObject _player = GameObject.FindGameObjectWithTag("Player"); //ele pega o jogador para poder referenciar o script
+		ShootScript = _player.GetComponent<BallShoot>();
+	}
 
-          
-    }
-    void OnCollisionEnter2D(Collision2D col)
-    {
-        if (col.gameObject.tag=="Player"&& ShootScript.IsPulling == true)
-        {    ShootScript.HasBall = true;
-            Destroy(gameObject);
-            
-        }
-    }
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player" && ShootScript.IsPulling == true)
+		{
+			ShootScript.HasBall = true;
+			Destroy(gameObject);
+
+		}
+	}
 
 
 }

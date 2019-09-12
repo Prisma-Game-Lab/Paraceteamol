891c06d4612e15ba31a464317a49ee27ed2704e1
raqueru <raquelolho@hotmail.com>
2019-09-12 18:53:55 -0300

    teste

---
+++ Paraceteamol/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GoalScript : MonoBehaviour
+{
+
+    public Text scoreText;
+    private int score;
+    [Tooltip("Colocar aqui os prefab da bola")]
+    public GameObject BallPrefab;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+    }
+
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Gols: " + score;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+        if (other.gameObject.tag == "Ball")
+        {
+
+            AddScore(1);
+            other.transform.position = new Vector2(0, 0);
+            
+            return;
+
+        }
+
+        }
+
+    }

70ab97c4ac1d04625063782b427cc988237d8591
Hugo Tonette <hugo.tonette@gmail.com>
2019-09-12 19:18:25 -0300

    fix pulo particulas funcionando

--- Paraceteamol/Assets/Scripts/BallPhysics.cs
+++ Paraceteamol/Assets/Scripts/BallPhysics.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class BallPhysics : MonoBehaviour
 {
-	public float StartSpeed = 1;
-	[HideInInspector]
-	public Vector2 Direction;
+	public float StartSpeed = 10;
+	//[HideInInspector]
+	public Vector2 Direction = Vector2.one;
 
-	private Vector2 _oldDirection;
-
-	private void Start()
-	{
-		Direction = Vector2.one.normalized;    // Direction (1,1) normalized - going top right
-	}
+	private bool _playerIsPulling;
 
 	private void FixedUpdate()
 	{
+		_playerIsPulling = GetComponent<BulletScript>().PlayerIsPulling;
+		if (_playerIsPulling)
+		{
+			//Direction = Vector2.zero;
+			GetComponent<Rigidbody2D>().simulated = false;
+		}
+		else
+		{
+			GetComponent<Rigidbody2D>().simulated = true;
+		}
+
 		transform.Translate(Direction * StartSpeed * Time.deltaTime);
 	}
 }
--- Paraceteamol/Assets/Scripts/BulletScript.cs
+++ Paraceteamol/Assets/Scripts/BulletScript.cs
@@ -6,24 +6,43 @@ public class BulletScript : MonoBehaviour
 {
 	[Tooltip("Velocidade com que a bola é lançada quando está com o player")]
 	public float Speed = 20;
+
 	[HideInInspector]
 	public Rigidbody2D Rb;
+	[HideInInspector]
+	public bool PlayerIsPulling = false;
 	
-	void Start()
-	{
-		//Rb.velocity = transform.up * Speed;
-	}
 
-	void OnCollisionEnter2D(Collision2D col)
+	private Vector2 _oldDirection;
+	private Vector2 _oldPosition;
+
+	void OnCollisionStay2D(Collision2D col)
 	{
-		if (col.gameObject.tag == "Player" && col.gameObject.GetComponentInChildren<AimController>().IsPulling)
+		if (col.gameObject.tag == "Player")
 		{
-			Debug.Log(col.gameObject.name + " touched ball");
-			//col.gameObject.GetComponentInChildren<AimController>().HasBall = true;
-			//AimScript.HasBall = true;
-			//Destroy(this.gameObject);
+			PlayerIsPulling = col.gameObject.GetComponentInChildren<AimController>().IsPulling;
+			if (PlayerIsPulling)
+			{
+				Debug.Log(col.gameObject.name + " touched ball");
+
+				//transform.position = col.gameObject.transform.position + new Vector3(0, 1, 0);
+
+				//GetComponent<Rigidbody2D>().isKinematic = false;
+				
+				//_oldDirection = GetComponent<BallPhysics>().Direction;  // Get old direction
+				//GetComponent<BallPhysics>().Direction = Vector2.zero;   // Set direction to zero.
+
+				// O player não segura mais a bola
+				//col.gameObject.GetComponentInChildren<AimController>().HasBall = true;
+				//AimScript.HasBall = true;
+				//Destroy(this.gameObject);
+			}
+			else
+			{
+				Debug.Log(col.gameObject.name + " released ball");
+				//GetComponent<BallPhysics>().Direction = new Vector2(1, 0);
+				//GetComponent<Rigidbody2D>().isKinematic = true;
+			}
 		}
 	}
-
-
 }

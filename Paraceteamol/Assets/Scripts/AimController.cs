728125719ef3564e98b2f95edecd80ec894e49ee
raqueru <raquelolho@hotmail.com>
2019-09-12 18:04:34 -0300

    mudança só pra ajeitar a direcao da bola quando é puxada e empurrada

--- Paraceteamol/Assets/Scripts/AimController.cs
+++ Paraceteamol/Assets/Scripts/AimController.cs
@@ -51,13 +51,14 @@ public class AimController : MonoBehaviour
 			if (Input.GetButton(_playerOne ? "p1_fire1" : "p2_fire1"))
 			{
 				col.transform.position = Vector3.MoveTowards(col.transform.position, transform.position, Strenght);
-				//col.GetComponent<BallPhysics>().Direction = transform.position;
+               
+				col.GetComponent<BallPhysics>().Direction = transform.position.normalized;
 			}
 			else if (Input.GetButton(_playerOne ? "p1_fire2" : "p2_fire2"))
 			{
 				col.transform.position = Vector2.MoveTowards(col.transform.position, -transform.position, Strenght);
-				col.GetComponent<BallPhysics>().Direction = new Vector2(-transform.position.x, -transform.position.y).normalized;
-			}
+                col.GetComponent<BallPhysics>().Direction = -transform.position.normalized;
+            }
 		}
 	}
 

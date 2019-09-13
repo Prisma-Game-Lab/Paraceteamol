1c855093ceed9b2b32769c36d014006d235d113e
Hugo Tonette <hugo.tonette@gmail.com>
2019-09-13 00:54:53 -0300

    Fix no gol

--- Paraceteamol/Assets/Scripts/GoalScript.cs
+++ Paraceteamol/Assets/Scripts/GoalScript.cs
@@ -1,48 +1,31 @@
-﻿using System.Collections;
-using UnityEngine.UI;
-using System.Collections.Generic;
+﻿using UnityEngine.UI;
 using UnityEngine;
-
 
 public class GoalScript : MonoBehaviour
 {
+	public Text scoreText;
 
-    public Text scoreText;
-    public int score;
-    [Tooltip("Colocar aqui os prefab da bola")]
-    public GameObject BallPrefab;
-
-    void Start()
-    {
-        score = 0;
-        UpdateScore();
-    }
-
-
-    public void AddScore(int newScoreValue)
-    {
-        score += newScoreValue;
-        UpdateScore();
-    }
-
-    void UpdateScore()
-    {
-        scoreText.text = "Gols: " + score;
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
+	[HideInInspector]
+	public int score = 0;
+	
+	void Start()
 	{
+		score = 0;
+		scoreText.text = "Gols: " + score;
+	}
 
-        if (other.gameObject.tag == "Ball")
-        {
+	public void AddScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		scoreText.text = "Gols: " + score;
+	}
 
-            AddScore(1);
-            other.transform.position =  new Vector2(0, 0);
-            
-            return;
-
-        }
-
-        }
-
-    }
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Ball")
+		{
+			AddScore(1);
+			other.transform.position = new Vector2(0, 0);
+		}
+	}
+}

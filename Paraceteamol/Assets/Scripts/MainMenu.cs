6b6dd76f219c39cbca5f20180aabf285e8deefd0
Sol <solmseda@gmail.com>
2019-09-13 01:00:16 -0300

    fix no Input Maneger

---
+++ Paraceteamol/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Load()
+    {
+        Debug.Log("Loading scene...");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}

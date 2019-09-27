e8dd2edf92705351228aae21eeab32b8d24d1de7
Hugo Tonette <hugo.tonette@gmail.com>
2019-09-26 22:57:13 -0300

    arrumando o codigo

--- Paraceteamol/Assets/Scripts/AnimationCode.cs
+++ Paraceteamol/Assets/Scripts/AnimationCode.cs
@@ -1,40 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AnimationCode : MonoBehaviour
+{
+	private Animator _animator;
 
-   {
-    private Animator _animator;
-     
+	void Start()
+	{
+		_animator = GetComponent<Animator>();
+	}
 
-    void Start()
-    {
-        _animator = GetComponent<Animator>();
-    }
+	public void Andar()
+	{
+		_animator.SetBool("movendo", true);
+	}
 
-    public void Andar()
-    {
+	public void PararDeAndar()
+	{
+		_animator.SetBool("movendo", false);
+	}
 
-        _animator.SetBool("movendo", true);
+	public void Pular()
+	{
+		_animator.SetTrigger("pulando");
+	}
 
-    }
-    public void PararDeAndar()
-    {
-       _animator.SetBool("movendo", false);
-    }
-
-
-
-    public void Pular()
-    {
-
-        _animator.SetTrigger("pulando");
-
-    }
-    public void PararDePular()
-    {
-        _animator.ResetTrigger("pulando");
-
-    }
+	public void PararDePular()
+	{
+		_animator.ResetTrigger("pulando");
+	}
 }

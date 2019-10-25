597955982a33b7ee2c3c5a60770685cc137f2200
raqueru <raquelolho@hotmail.com>
2019-10-25 15:28:12 -0300

    DEUS É MAIS, RECUPEREI AS ARENAS PERDIDAS

---
+++ Paraceteamol/Assets/Scripts/MapSelection.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MapSelection : MonoBehaviour
+{
+    public string Map;
+    public void ReturnMenu()
+    {
+        Debug.Log("Loading scene...");
+        SceneManager.LoadScene("Tela_inicial");
+    }
+    public void ChooseMap() {
+        SceneManager.LoadScene(Map);
+
+    }
+
+}

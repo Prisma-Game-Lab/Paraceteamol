b2da1e6beb31d73ce3c4c725afed5a00ce1d06da
Joao Victor Galindo <doublito@gmail.com>
2019-11-07 11:58:38 -0300

    + Portais que teleportam os players e a bola implementados * Dois Portais adicionados na GameScene

--- Paraceteamol/Assets/Scripts/BallPhysics.cs
+++ Paraceteamol/Assets/Scripts/BallPhysics.cs
@@ -3,7 +3,7 @@
 public class BallPhysics : MonoBehaviour
 {
 	[Tooltip("Forca com que a bola vai ser ricocheteada ")]
-	public float Strenght;
+	public float Strength;
 	public float StartSpeed;
 	[Tooltip("Direcao inicial da sua bola")]
 	public Vector2 Direction = Vector2.one;
---
+++ Paraceteamol/Assets/Scripts/Portal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+
+    public GameObject exitPoint;
+    public Portal exitPortal;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.CompareTag("Player") || col.gameObject.CompareTag("Ball")){
+            Debug.Log("Player entrou em portal");
+            col.transform.position = exitPortal.exitPoint.transform.position;
+        }
+    }
+
+}

0f888b6ba827767f227a4c3dbdd9d4b7fdff181e
Bernardo <behornerl@gmail.com>
2019-11-08 02:36:36 -0300

    Plataforma se movimentando entre waypoints corretamente, porém precisa configurar melhor para quando ela faz um movimento descendente

--- Paraceteamol/Assets/Scripts/MovingPlatform.cs
+++ Paraceteamol/Assets/Scripts/MovingPlatform.cs
@@ -8,15 +8,65 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] private Rigidbody2D platformRb;
     [SerializeField] private float moveSpeed = 0.5f;
 
+    [SerializeField] private Transform playersParent;
+    [Tooltip("Pontos pelos quais a plataforma se moverá")]
+    [SerializeField] List<Transform> waypoints;
+    private int currentWaypoint;
+    private Vector2 currentMoveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = waypoints[0].position;
+        currentWaypoint = 1;
+        currentMoveDirection = GetDirectionBetweenPoints(waypoints[currentWaypoint].position, transform.position);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        platformRb.MovePosition(new Vector2(platformRb.position.x - 1*moveSpeed, platformRb.position.y));
+        Debug.Log(waypoints[currentWaypoint].position);
+        if (Vector2.Distance(transform.position, waypoints[currentWaypoint].position) > 1)
+        {
+            platformRb.MovePosition(platformRb.position + currentMoveDirection * moveSpeed);
+        }
+        else
+        {
+            GetNextWaypoint();
+            currentMoveDirection = GetDirectionBetweenPoints(waypoints[currentWaypoint].position, transform.position);
+        }
+    }
+
+    // Returns a normalized vector indicating the direction the platform will move
+    private Vector2 GetDirectionBetweenPoints(Vector2 target, Vector2 currentPosition)
+    {
+        Vector2 direction = target - currentPosition;
+        return direction.normalized;
+    }
+
+    private int GetNextWaypoint()
+    {
+        currentWaypoint++;
+        if (currentWaypoint > waypoints.Count - 1)
+            currentWaypoint = 0;
+
+        return currentWaypoint;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.rigidbody.velocity = Vector2.zero;
+            collision.rigidbody.position = new Vector2(collision.rigidbody.position.x, transform.position.y + 2);
+        }
+    }
+    private void OnCollisionLeave2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.rigidbody.velocity = Vector2.zero;
+            collision.rigidbody.position = new Vector2(collision.rigidbody.position.x, transform.position.y);
+        }
     }
 }

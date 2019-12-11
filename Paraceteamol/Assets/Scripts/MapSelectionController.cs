bce7f3067c575faad0aef8a1c7c9aa742b71564c
htonette <hugo.tonette@gmail.com>
2019-12-11 18:47:54 -0300

    asdfgnm

--- Paraceteamol/Assets/Scripts/MapSelectionController.cs
+++ Paraceteamol/Assets/Scripts/MapSelectionController.cs
@@ -9,16 +9,13 @@ public class MapSelectionController : MonoBehaviour
 	[Space]
 	public GameObject ButtonSelector;
 
-	private GameObject[] _btnList;
-	private int _btnQty;
 	private int _currentNum = 0;
 	private bool _canSelect = true;
 
 	private void Start()
 	{
-		_btnList = ButtonList;
-		_btnQty = _btnList.Length;
-		ButtonSelector.transform.position = _btnList[0].transform.position;
+		ButtonList = ButtonList;
+		ButtonSelector.transform.position = ButtonList[0].transform.position;
 	}
 
 
@@ -42,11 +39,10 @@ public class MapSelectionController : MonoBehaviour
 		}
 
 		if (_currentNum < 0)
-			_currentNum = _btnList.Length - 1;
-		else if (_currentNum > _btnList.Length - 1)
+			_currentNum = ButtonList.Length - 1;
+		else if (_currentNum > ButtonList.Length - 1)
 			_currentNum = 0;
 
-		ButtonSelector.transform.position = Vector3.Lerp(ButtonSelector.transform.position,
-			_btnList[_currentNum].transform.position, 1f);
+		ButtonSelector.transform.position = Vector3.Lerp(ButtonSelector.transform.position, ButtonList[_currentNum].transform.position, 1f);
 	}
 }

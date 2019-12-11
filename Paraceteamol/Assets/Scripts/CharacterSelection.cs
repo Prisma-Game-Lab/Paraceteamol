e73e653da7f6ce076abb0c626181a7cf579d0a7e
Sol <solmseda@gmail.com>
2019-12-10 23:00:52 -0300

    Tela e script de seleção de personagem

---
+++ Paraceteamol/Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class CharacterSelection : MonoBehaviour
+{
+    private int selecterCharacterIndex;
+    private Color desiredColor;
+
+    [Header("List of character")]
+    [SerializeField] List<ChacterSelectObject> characterList = new List<ChacterSelectObject>();
+
+    [Header("UI References")]
+    //[SerializeField] private TextMeshProUGUI Character1_Name;
+    [SerializeField] private Image PLayer1Splash;
+    [SerializeField] private Image BackgroundColor;
+
+    [Header("Sounds")]
+    [SerializeField] private AudioClip backgroundMusic;
+
+    private void Start()
+    {
+        UpdateCharacterSelectionScreen();
+    }
+
+    private void LeftArrow()
+    {
+        selecterCharacterIndex--;
+        if (selecterCharacterIndex < 0)
+        {
+            selecterCharacterIndex = characterList.Count - 1;
+        }
+        UpdateCharacterSelectionScreen();
+    }
+
+    private void RightArrow()
+    {
+        selecterCharacterIndex++;
+        if (selecterCharacterIndex == characterList.Count)
+        {
+            selecterCharacterIndex = 0;
+        }
+        UpdateCharacterSelectionScreen();
+    }
+
+    private void UpdateCharacterSelectionScreen()
+    {
+        PLayer1Splash.sprite = characterList[selecterCharacterIndex].splash;
+        desiredColor = characterList[selecterCharacterIndex].characterColor;
+    }
+
+    [System.Serializable]
+    public class ChacterSelectObject{
+        public Sprite splash;
+        public string characterName;
+        public Color characterColor;
+   }
+}

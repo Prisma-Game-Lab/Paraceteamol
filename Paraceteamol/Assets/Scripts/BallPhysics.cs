f5a03e978dd85f6e4b9d788844b8dc6e7d47d6af
Hugo Tonette <hugo.tonette@gmail.com>
2019-12-19 14:57:27 -0300

    Bola volta pro campo caso ele fuja da arena

--- Paraceteamol/Assets/Scripts/BallPhysics.cs
+++ Paraceteamol/Assets/Scripts/BallPhysics.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 public class BallPhysics : MonoBehaviour
 {
-    [FMODUnity.EventRef]
-    public string inputsound;
-    [Header("Physics")]
+	[FMODUnity.EventRef]
+	public string inputsound;
+	[Header("Physics")]
 	[Tooltip("Velocidade da bola")]
 	public float Speed;
 	[Tooltip("Direcao inicial da bola")]
 	public Vector2 Direction = Vector2.one;
-    private GameObject GameManager;
-    private MatchScript _matchScript;
-    private GameObject playerCurrentlyHolding;
+	private GameObject GameManager;
+	private MatchScript _matchScript;
+	private GameObject playerCurrentlyHolding;
 
-    private AudioSource BallSound;
+	private AudioSource BallSound;
 	private Rigidbody2D _rb;
 	private Collider2D _col;
 	private float _startingSpeed;
 	private Collider2D _tempCol;
-    
+
 
 	#region StateMachine
 	public enum State
@@ -56,7 +56,7 @@ public class BallPhysics : MonoBehaviour
 
 	private void Awake()
 	{
-        GameManager = GameObject.Find("GameManeger");
+		GameManager = GameObject.Find("GameManeger");
 		_rb = GetComponent<Rigidbody2D>();
 		_rb.AddForce(Direction * Speed, ForceMode2D.Impulse);
 		BallSound = GetComponent<AudioSource>();
@@ -65,53 +65,34 @@ public class BallPhysics : MonoBehaviour
 	}
 
 	private void FixedUpdate()
-    {
-        Direction = _rb.velocity;
-      
+	{
+		Direction = _rb.velocity;
+
 		switch (state)
 		{
 			case State.Held:
-                _rb.velocity = Vector2.zero;
-				//_rb.isKinematic = true;
-				//_col.enabled = false;
-                
+				_rb.velocity = Vector2.zero;
 				break;
 			case State.Release:
-				//_rb.isKinematic = false;
-				//_col.enabled = true;
-              
 				Speed = _startingSpeed;
-             
-
-               
-				_rb.velocity = -ReleaseDirection(playerCurrentlyHolding.transform.position)*Speed ;
-                 
+				_rb.velocity = -ReleaseDirection(playerCurrentlyHolding.transform.position) * Speed;
 				state = State.Idle;
 				break;
- 
 		}
 	}
 
-    public void SetPlayerCurrentlyHolding(GameObject player)
-    {
-        playerCurrentlyHolding = player;
-    }
+	public void SetPlayerCurrentlyHolding(GameObject player)
+	{
+		playerCurrentlyHolding = player;
+	}
 
-
-    private void OnCollisionEnter2D(Collision2D col)
+	private void OnCollisionEnter2D(Collision2D col)
 	{
 		switch (state)
 		{
 			case State.Idle:
-               
-				if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Wall") || col.gameObject.CompareTag("Player")
-                    || col.gameObject.CompareTag("MovingPlatform"))
-				{ 
-               
+				if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Wall") || col.gameObject.CompareTag("Player") || col.gameObject.CompareTag("MovingPlatform"))
 					ReflectProjectile(_rb, col.contacts[0].normal);
-
-                  
-				}
 				break;
 			case State.Held:
 				if (col.gameObject.CompareTag("Player") || col.gameObject.CompareTag("Wall") || col.gameObject.CompareTag("MovingPlatform"))
@@ -122,32 +103,30 @@ public class BallPhysics : MonoBehaviour
 				break;
 		}
 	}
-    private void OnTriggerEnter2D(Collider2D col) {
-        if (col.gameObject == GameObject.Find("Time1"))
-        {
-        GameManager.GetComponent<MatchScript>().RedTeamGol();
-        }
-        else if (col.gameObject == GameObject.Find("Time2"))
-        {
-            GameManager.GetComponent<MatchScript>().BlueTeamGol();
-        }
-    
-    }
+
+	private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject == GameObject.Find("Time1"))
+		{
+			GameManager.GetComponent<MatchScript>().RedTeamGol();
+		}
+		else if (col.gameObject == GameObject.Find("Time2"))
+		{
+			GameManager.GetComponent<MatchScript>().BlueTeamGol();
+		}
+	}
+
 	private void ReflectProjectile(Rigidbody2D rb, Vector2 reflectVector)
 	{
 		Direction = Vector2.Reflect(Direction, reflectVector);
-         
-         
 		_rb.velocity = Speed * Direction.normalized;
-
-
-        FMODUnity.RuntimeManager.PlayOneShot (inputsound);
+		FMODUnity.RuntimeManager.PlayOneShot(inputsound);
 	}
 
 	private Vector2 ReleaseDirection(Vector2 playerPos)
 	{
 		Vector2 dir = gameObject.transform.position;
 		dir = playerPos - dir;
-        return dir.normalized;
+		return dir.normalized;
 	}
 }
---
+++ Paraceteamol/Assets/Scripts/BallRestrictor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRestrictor : MonoBehaviour
+{
+	public GameObject BallGameObject;
+
+	private Vector3 _ballPosition;
+	private Vector2 _ballVelocity;
+
+	private void Start()
+	{
+		_ballPosition = BallGameObject.transform.position;
+		_ballVelocity = BallGameObject.GetComponent<Rigidbody2D>().velocity;
+	}
+
+	private void OnTriggerExit2D(Collider2D col)
+	{
+		if(col.gameObject.tag == "Ball")
+		{
+			col.gameObject.transform.position = _ballPosition;
+			BallGameObject.GetComponent<Rigidbody2D>().velocity = _ballVelocity;
+		}
+	}
+}
